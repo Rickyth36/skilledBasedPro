014014fbcba607dc87e14c3a524bb75d770f1bbd
Ricky Thoudam <rickythoudam123@gmail.com>
2025-11-21 23:38:58 +0530

    landing UI update

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -1,10 +1,13 @@
 import { createContext, useEffect, useState } from "react";
 import { dummyCourses } from "../assets/assets";
+import { useNavigate } from "react-router-dom";
 
 export const AppContext = createContext();
 export const AppContextProvider = (props) => {
     const currency = import.meta.env.VITE_CURRENCY;
+    const navigate = useNavigate()
     const [allCourses, setAllCourses] = useState([]);
+    const [isEducator, setIsEducator] = useState(true);
 
     // fetch all courses
     const fetchAllCourses = async() =>{
@@ -14,9 +17,25 @@ export const AppContextProvider = (props) => {
         fetchAllCourses();
     },[]);
 
+    const calculateRating = (course) =>{
+        if(course.courseRatings.length === 0){
+            return 0;
+        }
+        let totalRatings = 0;
+        course.courseRatings.forEach((rating)=>
+            totalRatings += rating.rating
+        )
+        return totalRatings/course.courseRatings.length
+
+    }
+
     const value = {
         currency,
-        allCourses
+        allCourses,
+        navigate,
+        calculateRating,
+        isEducator,
+        setIsEducator
     }
     return (
         <AppContext.Provider value={value}>
